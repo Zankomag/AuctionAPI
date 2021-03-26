27731ff1a9d27952bd0ed15b1056db37f71684d9
Adam Evans <Zankomag@gmail.com>
2021-03-26 19:40:57 +0200

    Refactor

--- src/AuctionAPI.Web/Startup.cs
+++ src/AuctionAPI.Web/Startup.cs
@@ -14,79 +14,81 @@ using Newtonsoft.Json;
 using Serilog;
 
 namespace AuctionAPI.Web {
-    public class Startup {
-        public Startup(IConfiguration configuration) => Configuration = configuration;
 
-        public IConfiguration Configuration { get; }
+	public class Startup {
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
+		public IConfiguration Configuration { get; }
+		public Startup(IConfiguration configuration) => Configuration = configuration;
 
-            string connection = Configuration.GetConnectionString("AuctionDb");
-            services.AddDbContext<AuctionDbContext>(options => options.UseSqlServer(connection));
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services) {
 
-            
-            services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AuctionAPI", Version = "v1" });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme() {
-                    Description = @"JWT Authorization header using the Bearer scheme.
+			string connection = Configuration.GetConnectionString("AuctionDb");
+			services.AddDbContext<AuctionDbContext>(options => options.UseSqlServer(connection));
+
+
+			services.AddSwaggerGen(c => {
+				c.SwaggerDoc("v1", new OpenApiInfo {Title = "AuctionAPI", Version = "v1"});
+				c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+					Description = @"JWT Authorization header using the Bearer scheme.
 						Enter 'Bearer [token]' in the text input below",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement() {
-                    {
-                        new OpenApiSecurityScheme() {
-                            Reference = new OpenApiReference() {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                            Scheme = "oauth2",
-                            Name = "Bearer",
-                            In = ParameterLocation.Header
-                        },
-                        new List<string>()
-                    }
-                });
-            });
+					Name = "Authorization",
+					In = ParameterLocation.Header,
+					Type = SecuritySchemeType.ApiKey,
+					Scheme = "Bearer"
+				});
+				c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+					{
+						new OpenApiSecurityScheme {
+							Reference = new OpenApiReference {
+								Type = ReferenceType.SecurityScheme,
+								Id = "Bearer"
+							},
+							Scheme = "oauth2",
+							Name = "Bearer",
+							In = ParameterLocation.Header
+						},
+						new List<string>()
+					}
+				});
+			});
 
-            services.AddControllers()
-                .AddNewtonsoftJson(options => {
-                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-                })
-                .ConfigureApiBehaviorOptions(options => options.InvalidModelStateResponseFactory = context => {
-                    if(context.ModelState.ErrorCount > 0) {
-                        string messages = String.Join("; ", context.ModelState.Values
-                            .SelectMany(x => x.Errors).Select(x => x.ErrorMessage));
-                        return new BadRequestObjectResult(messages);
-                    }
-                    return new StatusCodeResult(500);
-                });
-        }
+			services.AddControllers()
+				.AddNewtonsoftJson(options => {
+					options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+					options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+				})
+				.ConfigureApiBehaviorOptions(options => options.InvalidModelStateResponseFactory = context => {
+					if(context.ModelState.ErrorCount > 0) {
+						string messages = String.Join("; ", context.ModelState.Values
+							.SelectMany(x => x.Errors).Select(x => x.ErrorMessage));
+						return new BadRequestObjectResult(messages);
+					}
+					return new StatusCodeResult(500);
+				});
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuctionAPI v1"));
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+			if(env.IsDevelopment()) {
+				app.UseDeveloperExceptionPage();
+				app.UseSwagger();
+				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuctionAPI v1"));
+			}
 
-            app.UseHttpsRedirection();
-            app.UseHsts();
+			app.UseHttpsRedirection();
+			app.UseHsts();
 
-            //To log only Warning or greater requests
-            //Set "Serilog.AspNetCore": "Warning" in Serilog MinimumLevel Config
-            app.UseSerilogRequestLogging();
+			//To log only Warning or greater requests
+			//Set "Serilog.AspNetCore": "Warning" in Serilog MinimumLevel Config
+			app.UseSerilogRequestLogging();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => endpoints.MapControllers());
-        }
-    }
+			app.UseEndpoints(endpoints => endpoints.MapControllers());
+		}
+	}
+
 }
