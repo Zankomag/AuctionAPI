b7bf21f899584175868946ee5b1ed57c19eff871
Adam Evans <Zankomag@gmail.com>
2021-03-28 01:06:35 +0200

    Add recursive category loading

--- src/AuctionAPI.Infrastructure/Repositories/AuctionItemCategoryRepository.cs
+++ src/AuctionAPI.Infrastructure/Repositories/AuctionItemCategoryRepository.cs
@@ -14,12 +14,40 @@ namespace AuctionAPI.Infrastructure.Repositories {
 		public AuctionItemCategoryRepository(DbContext context) : base(context) { }
 
 		/// <inheritdoc />
-		public async Task<IEnumerable<AuctionItemCategory>> GetAllWithDetailsAsync()
-			=> await GetAll().Include(x => x.ChildCategories).Where(x => x.ParentCategoryId == null).ToListAsync();
+		public async Task<IEnumerable<AuctionItemCategory>> GetAllWithDetailsAsync() {
+			//To optimize: First we get all categories in one query and then perform sorting locally
+			//(anyway we need all categories)
+
+			var rootCategories = await GetAllWithDetails()
+				.Where(x => x.ParentCategoryId == null)
+				.ToListAsync();
+			foreach(AuctionItemCategory category in rootCategories
+				.Where(category => category.ChildCategories != null && category.ChildCategories.Any())) {
+				category.ChildCategories = await GetChildren(category);
+			}
+			return rootCategories;
+		}
+		
+		
+
+		private async Task<ICollection<AuctionItemCategory>> GetChildren(AuctionItemCategory parent) {
+			var children = await GetAll().Where(x => x.ParentCategoryId == parent.Id)
+				.ToListAsync();
+
+			foreach(AuctionItemCategory child in children) {
+				child.ChildCategories = await GetChildren(child);
+			}
+			return children;
+		}
 
 		/// <inheritdoc />
-		public async Task<AuctionItemCategory> GetByIdWithDetailsAsync(int id)
-			=> await GetAll().Include(x => x.ChildCategories).FirstOrDefaultAsync(x => x.Id == id);
+		public async Task<AuctionItemCategory> GetByIdWithDetailsAsync(int id) {
+			var category =  await GetAllWithDetails().FirstOrDefaultAsync(x => x.Id == id);
+			if(category.ChildCategories != null && category.ChildCategories.Any()) {
+				category.ChildCategories = await GetChildren(category);
+			}
+			return category;
+		}
 
 		public IQueryable<AuctionItemCategory> GetAllWithDetails() => GetAll()
 			//.Include(x => x.ParentCategory)
