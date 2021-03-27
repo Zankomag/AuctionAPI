9638da64269bb322d7678f2a7131bad5d567d821
Adam Evans <Zankomag@gmail.com>
2021-03-27 18:20:43 +0200

    Create StringExtensions.cs

---
+++ src/AuctionAPI.Application/Extensions/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace AuctionAPI.Application.Extensions {
+
+	internal static class StringExtensions {
+		
+		///<summary>Escapes %, _, [, ^ and ~ chars with ~</summary>
+		public static string EscapeLikeText(string text) {
+
+			if(!text.Contains("%") && !text.Contains("_") && !text.Contains("[") && !text.Contains("^")) {
+				return text;
+			}
+			StringBuilder builder = new(text.Length);
+			foreach(char ch in text) {
+				switch(ch) {
+					case '%':
+					case '_':
+					case '[':
+					case '^':
+					case '~':
+						builder.Append('~');
+						break;
+				}
+				builder.Append(ch);
+			}
+			return builder.ToString();
+		}
+	}
+
+}
