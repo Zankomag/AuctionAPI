226eaa0a7cb73d0e559750b11f7b0baa5f7233b5
Adam Evans <Zankomag@gmail.com>
2021-03-27 14:36:32 +0200

    Add auctionItemCategoryRepository

---
+++ src/AuctionAPI.Core/Repositories/IAuctionItemCategoryRepository.cs
@@ -0,0 +1,7 @@
+﻿using AuctionAPI.Core.Entities;
+
+namespace AuctionAPI.Core.Repositories {
+
+	public interface IAuctionItemCategoryRepository : IRepository<AuctionItemCategory, int> { }
+
+}
---
+++ src/AuctionAPI.Infrastructure/Repositories/AuctionItemCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using AuctionAPI.Core.Entities;
+using AuctionAPI.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionAPI.Infrastructure.Repositories {
+
+	public class AuctionItemCategoryRepository : Repository<AuctionItemCategory, int>, IAuctionItemCategoryRepository {
+
+		/// <inheritdoc />
+		public AuctionItemCategoryRepository(DbContext context) : base(context) { }
+	}
+
+}
