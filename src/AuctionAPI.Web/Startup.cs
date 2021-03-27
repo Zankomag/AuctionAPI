094e88e17a3d98cf3da5b2d529bb13c26adca55d
Adam Evans <Zankomag@gmail.com>
2021-03-27 21:40:54 +0200

    Add comment

--- src/AuctionAPI.Web/Startup.cs
+++ src/AuctionAPI.Web/Startup.cs
@@ -21,6 +21,7 @@ namespace AuctionAPI.Web {
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services) {
+			//Configure all infrastructure services
 			services.AddInfrastructure(Configuration);
 
 			services.AddSwaggerGen(c => {
