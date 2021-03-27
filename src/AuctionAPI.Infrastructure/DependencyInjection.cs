fa2d79140d6f026e1885023cd9fffffba6921afa
Adam Evans <Zankomag@gmail.com>
2021-03-27 21:40:48 +0200

    Configure services setup in instrastructure

--- src/AuctionAPI.Infrastructure/DependencyInjection.cs
+++ src/AuctionAPI.Infrastructure/DependencyInjection.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AuctionAPI.Application.Mapping;
+using AuctionAPI.Application.Services;
+using AuctionAPI.Application.Services.Abstractions;
+using AuctionAPI.Core.Repositories;
 using AuctionAPI.Infrastructure.Data;
+using AuctionAPI.Infrastructure.Repositories;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,6 +20,13 @@ namespace AuctionAPI.Infrastructure {
 			string connection = config.GetConnectionString("AuctionDb");
 			services.AddDbContext<AuctionDbContext>(options => options.UseSqlServer(connection));
 
+			services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+			services.AddScoped<IAuctionItemCategoryService, AuctionItemCategoryService>();
+
+			var mapperConfig = new MapperConfiguration(x => x.AddProfile<EntityToModelProfile>());
+			services.AddSingleton(mapperConfig.CreateMapper());
+			
 			return services;
 		}
 	}
