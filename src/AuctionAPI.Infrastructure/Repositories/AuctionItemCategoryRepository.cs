6e93b9fd8951ff19698036cdd3de2b554073c7d3
Adam Evans <Zankomag@gmail.com>
2021-03-27 15:44:21 +0200

    Update AuctionItemCategoryRepository.cs

--- src/AuctionAPI.Infrastructure/Repositories/AuctionItemCategoryRepository.cs
+++ src/AuctionAPI.Infrastructure/Repositories/AuctionItemCategoryRepository.cs
@@ -1,5 +1,6 @@
 ﻿using AuctionAPI.Core.Entities;
 using AuctionAPI.Core.Repositories;
+using AuctionAPI.Infrastructure.Repositories.Generic;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuctionAPI.Infrastructure.Repositories {
