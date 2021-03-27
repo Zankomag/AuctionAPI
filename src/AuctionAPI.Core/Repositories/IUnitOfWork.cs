1b3b888b576f1fec693fa82808f183108139cee7
Adam Evans <Zankomag@gmail.com>
2021-03-27 14:36:40 +0200

    Add UnitOfWork

---
+++ src/AuctionAPI.Core/Repositories/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuctionAPI.Core.Entities;
+
+namespace AuctionAPI.Core.Repositories {
+    public interface IUnitOfWork {
+
+        IAuctionItemCategoryRepository AuctionItemCategoryRepository { get; }
+
+        Task<int> SaveAsync();
+    }
+}
---
+++ src/AuctionAPI.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using AuctionAPI.Core.Repositories;
+using AuctionAPI.Infrastructure.Data;
+
+namespace AuctionAPI.Infrastructure.Repositories {
+
+	public class UnitOfWork : IUnitOfWork {
+		protected AuctionDbContext Context;
+		private IAuctionItemCategoryRepository auctionItemCategoryRepository;
+
+		public UnitOfWork(AuctionDbContext context) => Context = context;
+
+		/// <inheritdoc />
+		public IAuctionItemCategoryRepository AuctionItemCategoryRepository
+			=> auctionItemCategoryRepository ??= new AuctionItemCategoryRepository(Context);
+
+		/// <inheritdoc />
+		public async Task<int> SaveAsync() => await Context.SaveChangesAsync();
+	}
+
+}
