0868ee0c1150c9b2232c7a05ceaf1cb0da5567c6
Adam Evans <Zankomag@gmail.com>
2021-03-27 20:45:50 +0200

    Create DependencyInjection.cs

---
+++ src/AuctionAPI.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuctionAPI.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionAPI.Infrastructure {
+	public static class DependencyInjection {
+		public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config) {
+			string connection = config.GetConnectionString("AuctionDb");
+			services.AddDbContext<AuctionDbContext>(options => options.UseSqlServer(connection));
+
+			return services;
+		}
+	}
+}
