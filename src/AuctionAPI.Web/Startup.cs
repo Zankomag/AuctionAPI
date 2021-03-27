f23d25534c52061247265a34e7fcbc8136148b0b
Adam Evans <Zankomag@gmail.com>
2021-03-27 20:45:58 +0200

    Update Startup.cs

--- src/AuctionAPI.Web/Startup.cs
+++ src/AuctionAPI.Web/Startup.cs
@@ -1,11 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using AuctionAPI.Infrastructure.Data;
+using AuctionAPI.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,10 +21,7 @@ namespace AuctionAPI.Web {
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services) {
-
-			string connection = Configuration.GetConnectionString("AuctionDb");
-			services.AddDbContext<AuctionDbContext>(options => options.UseSqlServer(connection));
-
+			services.AddInfrastructure(Configuration);
 
 			services.AddSwaggerGen(c => {
 				c.SwaggerDoc("v1", new OpenApiInfo {Title = "AuctionAPI", Version = "v1"});
