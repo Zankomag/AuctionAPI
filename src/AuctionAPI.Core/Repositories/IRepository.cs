6446cef528bcdc5549bca22bc943eef1869d9fb6
Adam Evans <Zankomag@gmail.com>
2021-03-27 13:55:29 +0200

    Add generic repository

---
+++ src/AuctionAPI.Core/Repositories/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuctionAPI.Core.Entities;
+
+namespace AuctionAPI.Core.Repositories {
+
+	public interface IRepository<TEntity, in TKey> where TEntity : Entity<TKey> {
+		IQueryable<TEntity> GetAll();
+
+		Task<List<TEntity>> GetAllAsync();
+
+		Task<TEntity> GetByIdAsync(TKey id);
+
+		Task AddAsync(TEntity entity);
+
+		void Update(TEntity entity);
+
+		void Delete(TEntity entity);
+
+		Task DeleteByIdAsync(TKey id);
+	}
+
+
+}
