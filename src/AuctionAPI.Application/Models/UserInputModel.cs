1087bc3be91a9b6f31e596143408479c009fa20a
Adam Evans <Zankomag@gmail.com>
2021-03-29 02:57:06 +0300

    Add user models

---
+++ src/AuctionAPI.Application/Models/UserInputModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuctionAPI.Application.Models {
+	public class UserInputModel {
+		[Required]
+		[StringLength(50)]
+		public string FirstName { get; set; }
+
+		[StringLength(50)]
+		public string LastName { get; set; }
+
+		//email is Username
+		[StringLength(50)]
+		[Required]
+		public string Email { get; set; }
+
+		[StringLength(64)]
+		[Required]
+		public string Password { get; set; }
+		
+	}
+}
---
+++ src/AuctionAPI.Application/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AuctionAPI.Application.Models.Generic;
+
+namespace AuctionAPI.Application.Models {
+
+	public class UserModel : Model<int> {
+		[Required]
+		[StringLength(50)]
+		public string FirstName { get; set; }
+
+		[StringLength(50)]
+		public string LastName { get; set; }
+
+		//email is Username
+		[StringLength(50)]
+		[Required]
+		public string Email { get; set; }
+
+		[StringLength(20)]
+		[Required]
+		public string Role { get; set; }
+	}
+
+}
