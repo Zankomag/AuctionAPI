d67bd2fb148d83416c33aae555aaac19f2482411
Adam Evans <Zankomag@gmail.com>
2021-03-28 17:04:59 +0300

    Add wrong input handling

--- src/AuctionAPI.Application/Services/AuctionItemCategoryService.cs
+++ src/AuctionAPI.Application/Services/AuctionItemCategoryService.cs
@@ -43,7 +43,8 @@ namespace AuctionAPI.Application.Services {
 		/// <inheritdoc />
 		public async Task<AuctionItemCategoryDetailedModel> GetByIdAsync(int id) {
 			try {
-				AuctionItemCategory auctionItemCategory = await workUnit.AuctionItemCategoryRepository.GetByIdWithDetailsAsync(id);
+				AuctionItemCategory auctionItemCategory =
+					await workUnit.AuctionItemCategoryRepository.GetByIdWithDetailsAsync(id);
 				return mapper.Map<AuctionItemCategoryDetailedModel>(auctionItemCategory);
 			} catch(Exception ex) {
 				logger.LogError(ex, ExceptionThrownInService);
@@ -56,6 +57,7 @@ namespace AuctionAPI.Application.Services {
 			try {
 				List<AuctionItemCategory> auctionItemCategories = await workUnit.AuctionItemCategoryRepository
 					.GetAll()
+
 					//If we'd used string.Contains, then expression wouldn't be case insensitive
 					//we use ~ as escape char because ToLikeString() escapes chars with ~
 					.Where(x => EF.Functions.Like(x.Name, categoryName.ToLikeString(), "~"))
@@ -77,6 +79,8 @@ namespace AuctionAPI.Application.Services {
 				await workUnit.SaveAsync();
 				model.Id = auctionItemCategory.Id;
 				return model;
+			} catch(DbUpdateException) {
+				return null;
 			} catch(Exception ex) {
 				logger.LogError(ex, ExceptionThrownInService);
 				throw;
@@ -92,8 +96,10 @@ namespace AuctionAPI.Application.Services {
 				AuctionItemCategory auctionItemCategory = mapper.Map<AuctionItemCategory>(model);
 				workUnit.AuctionItemCategoryRepository.Update(auctionItemCategory);
 				await workUnit.SaveAsync();
-				var result = await workUnit.AuctionItemCategoryRepository.GetByIdWithDetailsAsync(id);
+				AuctionItemCategory result = await workUnit.AuctionItemCategoryRepository.GetByIdWithDetailsAsync(id);
 				return mapper.Map<AuctionItemCategoryDetailedModel>(result);
+			} catch(DbUpdateException) {
+				return null;
 			} catch(Exception ex) {
 				logger.LogError(ex, ExceptionThrownInService);
 				throw;
@@ -101,10 +107,12 @@ namespace AuctionAPI.Application.Services {
 		}
 
 		/// <inheritdoc />
-		public async Task DeleteByIdAsync(int id) {
+		public async Task<bool> DeleteByIdAsync(int id) {
 			try {
-				await workUnit.AuctionItemCategoryRepository.DeleteByIdAsync(id);
+				if(!await workUnit.AuctionItemCategoryRepository.DeleteByIdAsync(id))
+					return false;
 				await workUnit.SaveAsync();
+				return true;
 			} catch(Exception ex) {
 				logger.LogError(ex, ExceptionThrownInService);
 				throw;
