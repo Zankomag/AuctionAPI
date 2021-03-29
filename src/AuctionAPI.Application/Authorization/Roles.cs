75538fe3003a2346e815899f31b16929ae344901
Adam Evans <Zankomag@gmail.com>
2021-03-29 04:39:39 +0300

    Remove unnecessary roles

--- src/AuctionAPI.Application/Authorization/Roles.cs
+++ src/AuctionAPI.Application/Authorization/Roles.cs
@@ -3,8 +3,6 @@
 	public static class Roles {
 		public const string Admin = "Admin";
 		public const string User = "User";
-		public const string Seller = "Seller";
-		public const string Bidder = "Bidder";
 	}
 
 }
