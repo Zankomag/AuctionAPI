a9f4d227529dd31ac7febb5016f53906889252fe
Adam Evans <Zankomag@gmail.com>
2021-03-29 04:17:01 +0300

    Create Roles.cs

---
+++ src/AuctionAPI.Application/Authorization/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace AuctionAPI.Application.Authorization {
+
+	public static class Roles {
+		public const string Admin = "Admin";
+		public const string User = "User";
+		public const string Seller = "Seller";
+		public const string Bidder = "Bidder";
+	}
+
+}
