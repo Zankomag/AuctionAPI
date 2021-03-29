9d6c418ecbd2c6d05921854984ea3b824bf129d9
Adam Evans <Zankomag@gmail.com>
2021-03-29 04:30:15 +0300

    Add UserService

---
+++ src/AuctionAPI.Application/Services/Abstractions/IUserService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AuctionAPI.Application.Models;
+
+namespace AuctionAPI.Application.Services.Abstractions {
+
+	public interface IUserService {
+		Task<IEnumerable<UserModel>> GetAllAsync();
+
+		Task<UserModel> GetByIdAsync(int id);
+
+		Task<UserModel> GetByEmailAsync(string email);
+
+		Task<UserModel> AddAsync(UserInputModel model);
+		
+		/// <returns>True on success</returns>
+		Task<bool> UpdateRoleAsync(int userId, string role);
+
+		/// <returns>True on success</returns>
+		Task<bool> DeleteAsync(int userId);
+	}
+
+}
---
+++ src/AuctionAPI.Application/Services/UserService.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using AuctionAPI.Application.Authorization;
+using AuctionAPI.Application.Models;
+using AuctionAPI.Application.Services.Abstractions;
+using AuctionAPI.Core.Entities;
+using AuctionAPI.Core.Repositories;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using static AuctionAPI.Application.Utils.LoggingMessages;
+using ValidationContext = System.ComponentModel.DataAnnotations.ValidationContext;
+
+namespace AuctionAPI.Application.Services {
+
+	public class UserService : IUserService {
+
+		private readonly IUnitOfWork workUnit;
+		private readonly IMapper mapper;
+		private readonly ILogger<UserService> logger;
+
+		public UserService(IUnitOfWork workUnit, IMapper mapper,
+			ILogger<UserService> logger) {
+			this.workUnit = workUnit;
+			this.mapper = mapper;
+			this.logger = logger;
+		}
+
+		/// <inheritdoc />
+		public async Task<IEnumerable<UserModel>> GetAllAsync() {
+			try {
+				IEnumerable<User> users = await workUnit.UserRepository.GetAllAsync();
+				return mapper.Map<IEnumerable<UserModel>>(users);
+			} catch(Exception ex) {
+				logger.LogError(ex, ExceptionThrownInService);
+				throw;
+			}
+		}
+
+		/// <inheritdoc />
+		public async Task<UserModel> GetByIdAsync(int id) {
+			try {
+				User user = await workUnit.UserRepository.GetByIdAsync(id);
+				return mapper.Map<UserModel>(user);
+			} catch(Exception ex) {
+				logger.LogError(ex, ExceptionThrownInService);
+				throw;
+			}
+		}
+
+		/// <inheritdoc />
+		public async Task<UserModel> GetByEmailAsync(string email) {
+			try {
+				User user = await workUnit.UserRepository.GetByEmailAsync(email);
+				return mapper.Map<UserModel>(user);
+			} catch(Exception ex) {
+				logger.LogError(ex, ExceptionThrownInService);
+				throw;
+			}
+		}
+
+		/// <inheritdoc />
+		public async Task<UserModel> AddAsync(UserInputModel model) {
+			if(!Validator.TryValidateObject(model, new ValidationContext(model), null, true))
+				return null;
+			try {
+				User user = mapper.Map<User>(model);
+				user.Role = Roles.User;
+				//TODO: do password hashing
+				await workUnit.UserRepository.AddAsync(user);
+				await workUnit.SaveAsync();
+				var result = mapper.Map<UserModel>(user);
+				return result;
+			} catch(DbUpdateException) {
+				return null;
+			} catch(Exception ex) {
+				logger.LogError(ex, ExceptionThrownInService);
+				throw;
+			}
+		}
+
+		/// <inheritdoc />
+		public async Task<bool> UpdateRoleAsync(int userId, string role) {
+			if(String.IsNullOrEmpty(role))
+				return false;
+			try {
+				workUnit.UserRepository.UpdateRoleAsync(userId, role);
+				await workUnit.SaveAsync();
+				return true;
+			} catch(DbUpdateException) {
+				return false;
+			} catch(Exception ex) {
+				logger.LogError(ex, ExceptionThrownInService);
+				throw;
+			}
+		}
+
+		/// <inheritdoc />
+		public async Task<bool> DeleteAsync(int userId) {
+			try {
+				if(!await workUnit.UserRepository.DeleteAsync(userId))
+					return false;
+				await workUnit.SaveAsync();
+				return true;
+			} catch(Exception ex) {
+				logger.LogError(ex, ExceptionThrownInService);
+				throw;
+			}
+		}
+	}
+
+}
