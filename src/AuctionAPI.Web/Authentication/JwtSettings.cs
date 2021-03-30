abb5149fbb382bb0ba3ce5b0a564e4253f779345
Adam Evans <Zankomag@gmail.com>
2021-03-31 01:39:04 +0300

    Update JwtSettings.cs

--- src/AuctionAPI.Web/Authentication/JwtSettings.cs
+++ src/AuctionAPI.Web/Authentication/JwtSettings.cs
@@ -1,7 +1,14 @@
-﻿namespace AuctionAPI.Web.Authentication {
+﻿using System.Text;
+
+namespace AuctionAPI.Web.Authentication {
 
 	public class JwtSettings {
+
 		public string Secret { get; set; }
+
+		public byte[] SecretBytes { get; }
+
+		public JwtSettings() => SecretBytes = Encoding.UTF8.GetBytes(Secret);
 	}
 
 }
