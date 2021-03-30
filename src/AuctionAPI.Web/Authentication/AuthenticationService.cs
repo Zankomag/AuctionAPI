9aa5bb6db57e8a768a967ecdc4049818a352a4fb
Adam Evans <Zankomag@gmail.com>
2021-03-31 02:09:25 +0300

    Add web api auth service

---
+++ src/AuctionAPI.Web/Authentication/AuthenticationService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AuctionAPI.Application.Services.Abstractions;
+using AuctionAPI.Web.Models;
+using AuctionAPI.Web.Services.Abstractions;
+using AutoMapper;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuctionAPI.Web.Authentication {
+
+	public class AuthenticationService : IAuthenticationService {
+		private readonly IUserService userService;
+		private readonly IMapper mapper;
+		private readonly JwtSettings jwtSettings;
+
+		public AuthenticationService(IUserService userService, IMapper mapper,
+			IOptions<JwtSettings> jwtSettingsOptions) {
+			this.userService = userService;
+			this.mapper = mapper;
+			jwtSettings = jwtSettingsOptions.Value;
+		}
+
+		/// <inheritdoc />
+		public async Task<TokenModel> GetToken(string email, string password) {
+			var userModel = await userService.GetAuthorizationInfoByEmailAndPasswordAsync(email, password);
+			if(userModel == null)
+				return null;
+			var user = mapper.Map<UserIdentity>(userModel);
+
+			var expiration = DateTime.Now.AddDays(1);
+			
+			var claims = new List<Claim> {
+				new(ClaimTypes.Role, user.Role),
+				new(ClaimTypes.NameIdentifier, user.Id.ToString())
+			};
+
+			var signingKey = new SymmetricSecurityKey(jwtSettings.SecretBytes);
+
+			var token = new JwtSecurityToken(expires: expiration,
+				claims: claims,
+				signingCredentials: new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256));
+			
+			return new TokenModel() {
+				Expiration = expiration,
+				Token = new JwtSecurityTokenHandler().WriteToken(token)
+			};
+
+
+		}
+	}
+
+}
