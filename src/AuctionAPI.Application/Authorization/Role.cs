b4511edac2fd36d5d6199ccb10e2dd17aea696e1
Adam Evans <Zankomag@gmail.com>
2021-03-31 00:43:19 +0300

    Add todo commend

--- src/AuctionAPI.Application/Authorization/Role.cs
+++ src/AuctionAPI.Application/Authorization/Role.cs
@@ -1,6 +1,8 @@
 ﻿namespace AuctionAPI.Application.Authorization {
 
-	public static class Roles {
+	//TODO roles need to be saved in db and one user can have many roles
+	//and all roles have to be retrieved from db on authentication
+	public static class Role {
 		public const string Admin = "Admin";
 		public const string User = "User";
 	}
