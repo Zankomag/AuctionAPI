33c6296802dc5d694078292c7f7dcac1ad49c533
Adam Evans <Zankomag@gmail.com>
2021-03-30 23:23:21 +0300

    Add authentication controller

---
+++ src/AuctionAPI.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using AuctionAPI.Application.Models;
+using AuctionAPI.Web.Models;
+using AuctionAPI.Web.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionAPI.Web.Controllers {
+
+	[ApiController]
+	[Route("api/token")]
+	[Produces("application/json")]
+	public class AuthenticationController : ControllerBase {
+		private readonly IAuthenticationService authenticationService;
+
+		public AuthenticationController(IAuthenticationService authenticationService)
+			=> this.authenticationService = authenticationService;
+
+		// POST /api/token
+		[HttpPost]
+		public async Task<ActionResult<TokenModel>> Authenticate([FromBody] AuthenticationModel model) {
+			var tokenModel = await authenticationService.GetToken(model.Email, model.Password);
+
+			if(tokenModel == null)
+				return BadRequest("Wrong username or password");
+
+			return Ok(tokenModel);
+		}
+	}
+
+}
---
+++ src/AuctionAPI.Web/Services/Abstractions/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using AuctionAPI.Web.Models;
+
+namespace AuctionAPI.Web.Services.Abstractions {
+
+	public interface IAuthenticationService {
+		/// <returns>JWT Token on success or null if email or password is wrong</returns>
+		Task<TokenModel> GetToken(string email, string password);
+	}
+
+}
