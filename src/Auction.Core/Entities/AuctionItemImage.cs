79bfd9ab20671b03bb07819d957e529bd3362e56
Adam Evans <Zankomag@gmail.com>
2021-04-02 00:39:33 +0300

    Add todo

--- src/Auction.Core/Entities/AuctionItemImage.cs
+++ src/Auction.Core/Entities/AuctionItemImage.cs
@@ -2,6 +2,8 @@
 
 	public class AuctionItemImage : Entity<int> {
 		public int AuctionItemId { get; set; }
+		
+		//TODO Try to implement filestream https://stackoverflow.com/a/44540717/11101834
 		public byte[] Image { get; set; }
 		
 		public AuctionItem AuctionItem { get; set; }
