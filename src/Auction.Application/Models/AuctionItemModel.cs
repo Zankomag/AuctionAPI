e754357a1332b61645ddfa7c537508b00b5f0eb7
Adam Evans <Zankomag@gmail.com>
2021-04-02 16:58:07 +0300

    Create AuctionItemModel.cs

---
+++ src/Auction.Application/Models/AuctionItemModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Auction.Core.Entities;
+
+namespace Auction.Application.Models {
+
+	public class AuctionItemModel : AuctionItemInputModel {
+		public AuctionItemStatusCodeId AuctionItemStatusCodeId { get; set; }
+
+		public DateTime? ActualCloseDate { get; set; }
+		public int? WinningBidId { get; set; }
+
+
+		public IEnumerable<FileModel> Images { get; set; }
+		public BidModel WinningBid { get; set; }
+		public IEnumerable<BidModel> Bids { get; set; }
+		public UserModel Seller { get; set; }
+		public AuctionItemCategoryInputModel AuctionItemCategory { get; set; }
+	}
+
+}
