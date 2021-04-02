fcdf2a59f01dfa8990f7f15510d2342c419febe2
Adam Evans <Zankomag@gmail.com>
2021-04-02 14:32:34 +0300

    Create AuctionItemInputModel.cs

---
+++ src/Auction.Application/Models/AuctionItemInputModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Auction.Application.Models.Generic;
+
+namespace Auction.Application.Models {
+
+	public class AuctionItemInputModel : Model<int> {
+		public int SellerId { get; set; }
+		public int? AuctionItemCategoryId { get; set; }
+
+		[Required]
+		[StringLength(256)]
+		public string Name { get; set; }
+
+		[StringLength(1024)]
+		public string Description { get; set; }
+
+		public DateTime StartDate { get; set; }
+		public DateTime PlannedCloseDate { get; set; }
+
+		[Column(TypeName = "decimal(18,2)")]
+		public decimal StartingPrice { get; set; }
+	}
+
+}
