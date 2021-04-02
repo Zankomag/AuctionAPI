75ef28b5ae5c8b71b634ccab83480274964f7295
Adam Evans <Zankomag@gmail.com>
2021-04-02 16:58:02 +0300

    Add bid models

---
+++ src/Auction.Application/Models/BidInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Auction.Application.Models.Generic;
+
+namespace Auction.Application.Models {
+
+	public class BidInputModel : Model<int> {
+		public int AuctionItemId { get; set; }
+
+		[RegularExpression(@"^\d+\.\d{0,2}$")]
+		[Range(0, 9999999999999999.99)]
+		public decimal Price { get; set; }
+	}
+
+}
---
+++ src/Auction.Application/Models/BidModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Auction.Application.Models {
+
+	public class BidModel : BidInputModel {
+		public int BidderId { get; set; }
+
+		public UserModel Bidder { get; set; }
+		public AuctionItemModel AuctionItem { get; set; }
+	}
+
+}
