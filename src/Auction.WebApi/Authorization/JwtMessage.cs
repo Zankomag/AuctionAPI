caadbc23e18b37ad193fd7a739e1cf1c071eb8c8
Adam Evans <Zankomag@gmail.com>
2021-04-03 13:21:04 +0300

    Add jwt message constants

---
+++ src/Auction.WebApi/Authorization/JwtMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Auction.WebApi.Authorization {
+
+	public static class JwtMessage {
+		public const string InvalidToken = "Invalid token";
+		public const string SubPropertyDoesntExist = InvalidToken + ": 'sub' property doesn't exist";
+		public const string SubPropertyIsNotInteger = InvalidToken + ": 'sub' property is not an integer";
+	}
+
+}
