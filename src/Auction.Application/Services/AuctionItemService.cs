47ac17c88627740087a90d1632cc644a0729c414
Adam Evans <Zankomag@gmail.com>
2021-04-04 00:28:05 +0300

    refactor

--- src/Auction.Application/Services/AuctionItemService.cs
+++ src/Auction.Application/Services/AuctionItemService.cs
@@ -66,9 +66,9 @@ namespace Auction.Application.Services {
 		}
 
 		/// <inheritdoc />
-		public async Task<int> GetOwnerId(int id) {
+		public async Task<int?> GetOwnerId(int id) {
 			try {
-				int ownerId = await workUnit.AuctionItemRepository.GetAll()
+				int? ownerId = await workUnit.AuctionItemRepository.GetAll()
 					.Where(x => x.Id == id)
 					.Select(x => x.SellerId)
 					.FirstOrDefaultAsync();
@@ -80,13 +80,8 @@ namespace Auction.Application.Services {
 		}
 
 		/// <inheritdoc />
-		public async Task<bool> IsUserOwner(int auctionItemId, int userId) {
-			//TODO don't use UserRepository.UserExist, use service 
-			//user service can cache existing users that it already checked
-			if(!await workUnit.UserRepository.UserExists(userId))
-				return false;
-			return userId == await GetOwnerId(auctionItemId);
-		}
+		public async Task<bool> IsUserOwner(int auctionItemId, int userId) 
+			=> userId == await GetOwnerId(auctionItemId);
 
 		/// <inheritdoc />
 		public async Task<AuctionItemInputModel> AddAsync(AuctionItemInputModel model) {
