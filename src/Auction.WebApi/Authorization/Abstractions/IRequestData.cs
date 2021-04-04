65244f525c08722599d5e5a9d0e3c10b2d151fcc
Adam Evans <Zankomag@gmail.com>
2021-04-04 16:43:45 +0300

    Add request Data holder

---
+++ src/Auction.WebApi/Authorization/Abstractions/IRequestData.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+using Microsoft.AspNetCore.Routing;
+
+namespace Auction.WebApi.Authorization.Abstractions {
+
+	public interface IRequestData {
+		/// <summary>
+		///     'sub' field of JWT
+		/// </summary>
+		string? UserIdString { get; set; }
+
+		/// <summary>
+		///     'sub' field of JWT
+		/// </summary>
+		int? UserId { get; set; }
+
+		RouteData RouteData { get; }
+
+		string? RouteIdString { get; }
+	}
+
+}
---
+++ src/Auction.WebApi/Authorization/RequestData.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+using Auction.WebApi.Authorization.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Auction.WebApi.Authorization {
+
+	public class RequestData : IRequestData {
+		private readonly IHttpContextAccessor httpContextAccessor;
+		private RouteData? routeData;
+		
+		public RequestData(IHttpContextAccessor httpContextAccessor)
+			=> this.httpContextAccessor = httpContextAccessor;
+
+		/// <inheritdoc />
+		public string? UserIdString { get; set; }
+
+		/// <inheritdoc />
+		public int? UserId { get; set; }
+
+		/// <inheritdoc />
+		public RouteData RouteData
+			=> routeData ??= httpContextAccessor.HttpContext!.GetRouteData();
+
+		/// <inheritdoc />
+		public string? RouteIdString => RouteData.Values["id"] as string;
+	}
+
+}
