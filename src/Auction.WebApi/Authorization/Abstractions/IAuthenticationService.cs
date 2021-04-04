dca4d4b329ec07051855f4ee8941fd6206ba88d8
Adam Evans <Zankomag@gmail.com>
2021-04-04 17:00:08 +0300

    Add XML docs

--- src/Auction.WebApi/Authorization/Abstractions/IAuthenticationService.cs
+++ src/Auction.WebApi/Authorization/Abstractions/IAuthenticationService.cs
@@ -3,6 +3,9 @@ using Auction.WebApi.Authorization.Types;
 
 namespace Auction.WebApi.Authorization.Abstractions {
 
+	/// <summary>
+	/// Provides methods to retrieve API authentication credentials (like JWT)
+	/// </summary>
 	public interface IAuthenticationService {
 		/// <returns>JWT Token on success or null if email or password is wrong</returns>
 		Task<TokenModel> GetToken(string email, string password);
