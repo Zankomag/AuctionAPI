b04b3623d9c1debc9e2cba1f803f7bc8e1e488fa
Adam Evans <Zankomag@gmail.com>
2021-04-05 19:34:50 +0300

    rename auth message constants

--- src/Auction.WebApi/Authorization/Constants/AuthenticationMessage.cs
+++ src/Auction.WebApi/Authorization/Constants/AuthenticationMessage.cs
@@ -1,6 +1,6 @@
 ﻿namespace Auction.WebApi.Authorization.Constants {
 
-	public static class JwtMessage {
+	public static class AuthenticationMessage {
 		public const string InvalidToken = "Invalid token";
 		public const string SubPropertyDoesntExist = InvalidToken + ": 'sub' property doesn't exist";
 		public const string SubPropertyIsNotInteger = InvalidToken + ": 'sub' property is not an integer";
