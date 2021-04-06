700c59a5721ad2b8251d97b0ab5f3a611b70cb75
Adam Evans <Zankomag@gmail.com>
2021-04-07 00:20:19 +0300

    Fix capitalization on Id field

--- src/Auction.Application/Models/Generic/Model.cs
+++ src/Auction.Application/Models/Generic/Model.cs
@@ -1,14 +1,16 @@
 ﻿using Newtonsoft.Json;
 
+// ReSharper disable InconsistentNaming
+
 namespace Auction.Application.Models.Generic {
 
 	public class Model<TKey> {
 		[JsonIgnore]
 		public TKey Id { get; set; }
-		
+
 		//This kludge is used to prevent Id field be Deserialized when posting input model
 		//Id field should appear first, so it should have order -2 (https://stackoverflow.com/a/14035431/11101834, https://www.newtonsoft.com/json/help/html/JsonPropertyOrder.htm)
-		[JsonProperty(PropertyName = nameof(Id), Order = -2)]
+		[JsonProperty(Order = -2)]
 		private TKey id => Id;
 	}
 
