dcaef6ee06701f2ce37a5296d25f4eaece90c956
Adam Evans <Zankomag@gmail.com>
2021-04-07 18:26:31 +0300

    Add bidder repository

---
+++ src/Auction.Core/Repositories/IBidRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Auction.Core.Entities;
+using Auction.Core.Repositories.Generic;
+
+namespace Auction.Core.Repositories {
+
+	public interface IBidRepository : IRepository<Bid, int> {
+		/// <summary>
+		/// Returns query without tracking 
+		/// </summary>
+		IQueryable<Bid> GetAll();
+
+		Task<Bid> GetByIdWithDetailsAsync(int id);
+		
+		Task<IEnumerable<Bid>> GetByAuctionItemIdWithDetailsAsync(int auctionItemId);
+
+		Task AddAsync(Bid entity);
+	}
+
+}
---
+++ src/Auction.Infrastructure/Repositories/BidRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Auction.Core.Entities;
+using Auction.Core.Repositories;
+using Auction.Infrastructure.Repositories.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auction.Infrastructure.Repositories {
+
+	public class BidRepository : Repository<Bid, int>, IBidRepository {
+		/// <inheritdoc />
+		public BidRepository(DbContext context) : base(context) { }
+
+		/// <inheritdoc />
+		public IQueryable<Bid> GetAll() => DbSet.AsNoTracking();
+
+		/// <inheritdoc />
+		public async Task<Bid> GetByIdWithDetailsAsync(int id)
+			=> await GetAll()
+				.Include(x => x.AuctionItem)
+				.Include(x => x.Bidder)
+				.FirstOrDefaultAsync(x => x.Id == id);
+
+		/// <inheritdoc />
+		public async Task<IEnumerable<Bid>> GetByAuctionItemIdWithDetailsAsync(int auctionItemId)
+			=> await GetAll()
+				.Include(x => x.Bidder)
+				.Where(x => x.AuctionItemId == auctionItemId)
+				.ToListAsync();
+
+		/// <inheritdoc />
+		public async Task AddAsync(Bid entity) => await DbSet.AddAsync(entity);
+	}
+
+}
