a5ac66dff7b9b6f89f31d04e0ae0c94b83e431e2
Adam Evans <Zankomag@gmail.com>
2021-04-08 00:22:15 +0300

    Add fileExtension field to image

--- src/Auction.Core/Entities/AuctionItemImage.cs
+++ src/Auction.Core/Entities/AuctionItemImage.cs
@@ -1,11 +1,16 @@
-﻿namespace Auction.Core.Entities {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Auction.Core.Entities {
 
 	public class AuctionItemImage : Entity<int> {
 		public int AuctionItemId { get; set; }
 		
 		//TODO Try to implement filestream https://stackoverflow.com/a/44540717/11101834
 		public byte[] Image { get; set; }
-		
+
+		[Required]
+		public string FileExtension { get; set; }
+
 		public AuctionItem AuctionItem { get; set; }
 	}
 
