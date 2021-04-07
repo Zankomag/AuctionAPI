2bea1f0259f5d397a395416e0154ec6e029ceb4d
Adam Evans <Zankomag@gmail.com>
2021-04-07 21:01:11 +0300

    Add exceptRequirement

---
+++ src/Auction.WebApi/Authorization/Requirements/Handlers/ExceptRequirementHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+// ReSharper disable InheritdocConsiderUsage
+
+namespace Auction.WebApi.Authorization.Requirements.Handlers {
+
+	/// <summary>
+	///     This handler must be registered after or Handlers of combined with Not policies
+	/// </summary>
+	public class ExceptRequirementHandler : AuthorizationHandler<IExceptRequirement> {
+
+		protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+			IExceptRequirement requirement) {
+
+			if(!context.HasFailed) {
+				if(context.PendingRequirements.Any(x => x is IExceptRequirement)) {
+					context.Succeed(requirement);
+				} else {
+					context.Fail();
+				}
+			}
+			return Task.CompletedTask;
+		}
+	}
+
+}
---
+++ src/Auction.WebApi/Authorization/Requirements/IExceptRequirement.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+// ReSharper disable InheritdocConsiderUsage
+
+namespace Auction.WebApi.Authorization.Requirements {
+
+	/// <summary>
+	///     This requirement fails if requirement other handlers succeed requirement inherited from this
+	/// </summary>
+	public interface IExceptRequirement : IAuthorizationRequirement { }
+
+}
